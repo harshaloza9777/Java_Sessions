0cd11d4832a7e5dfb0ac6a3189f409bbe9c2baf7
harshal <harshal9777@gmail.com>
2024-07-11 15:27:01 +0530

    first commit

---
+++ src/test/Numbers.java
@@ -0,0 +1,28 @@
+package test;
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(10004);
+		System.out.println(43+76);
+		System.out.println(33*14);
+		System.out.println(65/6);
+	    		
+		
+		int x = 5;
+		int y = 21;
+		
+		System.out.println(x);
+		System.out.println(y);
+		
+		y =35;
+		System.out.println(y);
+		System.out.println(x+y);
+		
+		System.out.println("x+y");
+		System.out.println(2* x+ 3* y);
+	}
+
+}
---
+++ src/test/StringExample.java
@@ -0,0 +1,27 @@
+package test;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Harshal");
+		System.out.println("Harshal Oza");
+		
+		String Fname = "Harshal";
+		String Lname = "Oza";
+		
+		System.out.println(Fname);
+		System.out.println(Lname);
+		
+		String Fullname = Fname +" "+ Lname;
+		System.out.println(Fullname);
+		
+		System.out.println(Fullname.length());
+		System.out.println(Fullname.toUpperCase());
+		System.out.println(Fullname.toLowerCase());
+		
+		System.out.println(Fullname.charAt(Fullname. length() -1));	
+	}
+
+}
